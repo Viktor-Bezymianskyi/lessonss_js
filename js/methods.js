360112b3d00f1676557c79a4be98575516613a49
Viktor-Bezymianskyi <photoprint10x20@gmail.com>
2023-04-14 11:55:53 +0300

    Lessons 28 Methods

---
+++ js/methods.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const str = 'test';
+const arr = [1, 2, 3,];
+
+console.log(arr.length);
+/* console.log(str[2] = 'd'); */
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello world';
+
+/* console.log(logg.slice(6, 11)); */
+console.log(logg.substring(6, 11));
+
+
+const num = 12.4;
+
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
