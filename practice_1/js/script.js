9e5ff31e2c7ebbc1811bbb9a00c08497c600b1a7
Viktor-Bezymianskyi <photoprint10x20@gmail.com>
2023-04-26 19:03:16 +0300

    practice_1

---
+++ practice_1/js/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* a = 15;
+console.log(a); */
+
+let number = 5;
+const leftBorderWidth = 1;
+
+
+number = 10;
+console.log(number);
+
+const obj = {
+	a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+var name = 'Ivan';
+
+{
+	var result = 50;
+}
+
+console.log(result);
+
+alert(5);
+[].push('a');
+
+const vehicleBodyWidth = 5000;
+const vehicleBodyLength = 4000;
+
+console.log('Width car body: ' + vehicleBodyWidth + ', length: ' + vehicleBodyLength);
+
+/* ['vss', 'dddd', 'wwww'].map(a => ); */
+
+//Request
+/* data
+response */
+
+// snake_case
+// UPPER_SNAKE_CASE
+
+const COLOR_RED = '#F00';
+
+
+
+
